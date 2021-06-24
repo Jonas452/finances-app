af62b3ca88a61286557776448a1a87c4e9b8a9d7
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2021-06-24 16:18:30 -0300

    feat(cache): create database entities for expense and income

---
+++ app/src/main/java/com/jonas/financesapp/cache/entity/IncomeEntity.kt
@@ -0,0 +1,31 @@
+package com.jonas.financesapp.cache.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.jonas.financesapp.cache.entity.IncomeEntity.Companion.TABLE_NAME
+import java.math.BigDecimal
+import java.util.*
+
+@Entity(tableName = TABLE_NAME)
+data class IncomeEntity(
+    @PrimaryKey @ColumnInfo(name = COLUMN_ID) val id: Long?,
+    @ColumnInfo(name = COLUMN_AMOUNT) val amount: BigDecimal,
+    @ColumnInfo(name = COLUMN_DESCRIPTION) val description: String,
+    @ColumnInfo(name = COLUMN_DATE) val date: Date,
+    @ColumnInfo(name = COLUMN_RECEIVED) val received: Boolean,
+) {
+
+    companion object {
+
+        const val TABLE_NAME = "income"
+
+        const val COLUMN_ID = "id"
+        const val COLUMN_AMOUNT = "amount"
+        const val COLUMN_DESCRIPTION = "description"
+        const val COLUMN_DATE = "date"
+        const val COLUMN_RECEIVED = "received"
+
+    }
+
+}
