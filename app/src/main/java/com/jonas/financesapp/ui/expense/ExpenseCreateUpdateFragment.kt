4fc7618686b6d71cbb990e5c9d42f166a917b35c
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2021-06-25 19:32:27 -0300

    feat(app): impl insert and update of expense

--- app/src/main/java/com/jonas/financesapp/ui/expense/ExpenseCreateUpdateFragment.kt
+++ app/src/main/java/com/jonas/financesapp/ui/expense/ExpenseCreateUpdateFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.jonas.financesapp.R
 import com.jonas.financesapp.databinding.FragmentExpenseCreateUpdateBinding
+import com.jonas.financesapp.util.DateUtils
+import com.jonas.financesapp.util.EventObserver
+import com.jonas.financesapp.util.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
+import java.sql.Date
 
 @AndroidEntryPoint
 class ExpenseCreateUpdateFragment : Fragment() {
@@ -22,7 +31,89 @@ class ExpenseCreateUpdateFragment : Fragment() {
         binding = FragmentExpenseCreateUpdateBinding.inflate(inflater, container, false)
         binding.viewModel = viewModel
 
+        setupListeners()
+        setupObservers()
+
         return binding.root
+    }
+
+    private fun setupListeners() {
+        binding.dateInput.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                requireActivity().hideKeyboard()
+                showDatePicker()
+            }
+            binding.dateInput.clearFocus()
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.expenseCreateUpdateEvent.observe(viewLifecycleOwner, EventObserver { state ->
+            when (state) {
+                ExpenseCreateUpdateViewModel.ExpenseCreateUpdateState.ErrorInserting -> {
+                    Toast.makeText(
+                        requireActivity().applicationContext,
+                        R.string.error_inserting_expense,
+                        Toast.LENGTH_LONG,
+                    ).show()
+                }
+                ExpenseCreateUpdateViewModel.ExpenseCreateUpdateState.ErrorUpdating -> {
+                    Toast.makeText(
+                        requireActivity().applicationContext,
+                        R.string.error_updating_expense,
+                        Toast.LENGTH_LONG,
+                    ).show()
+                }
+                ExpenseCreateUpdateViewModel.ExpenseCreateUpdateState.InvalidData -> {
+                    //TODO improve this feedback to the user
+                    Toast.makeText(
+                        requireActivity().applicationContext,
+                        R.string.error_fill_required_fields,
+                        Toast.LENGTH_LONG,
+                    ).show()
+                }
+                ExpenseCreateUpdateViewModel.ExpenseCreateUpdateState.SuccessInserting -> {
+                    Toast.makeText(
+                        requireActivity().applicationContext,
+                        R.string.success_inserting_expense,
+                        Toast.LENGTH_LONG,
+                    ).show()
+                    navigateBack()
+                }
+                ExpenseCreateUpdateViewModel.ExpenseCreateUpdateState.SuccessUpdating -> {
+                    Toast.makeText(
+                        requireActivity().applicationContext,
+                        R.string.success_updating_expense,
+                        Toast.LENGTH_LONG,
+                    ).show()
+                    navigateBack()
+                }
+            }
+        })
+    }
+
+    private fun navigateBack() {
+        val action =
+            ExpenseCreateUpdateFragmentDirections.actionExpenseCreateUpdateFragmentToDashboardFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun showDatePicker() {
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText(getString(R.string.select_date))
+                .build()
+
+        datePicker.show(
+            (requireActivity() as AppCompatActivity).supportFragmentManager,
+            "datePicker"
+        )
+
+        datePicker.addOnPositiveButtonClickListener {
+            binding.dateInput.setText(
+                DateUtils.formatDate(Date(it), DateUtils.DAY_MONTH_YEAR_FORMAT_DATE_WITHOUT_TIME)
+            )
+        }
     }
 
 }
--- app/src/main/java/com/jonas/financesapp/ui/expense/ExpenseCreateUpdateViewModel.kt
+++ app/src/main/java/com/jonas/financesapp/ui/expense/ExpenseCreateUpdateViewModel.kt
@@ -1,10 +1,101 @@
 package com.jonas.financesapp.ui.expense
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jonas.financesapp.di.IOContext
+import com.jonas.financesapp.model.ExpenseItem
+import com.jonas.financesapp.usecase.expense.InsertExpenseUseCase
+import com.jonas.financesapp.usecase.expense.UpdateExpenseUseCase
+import com.jonas.financesapp.util.DateUtils
+import com.jonas.financesapp.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.math.BigDecimal
+import java.util.*
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 @HiltViewModel
-class ExpenseCreateUpdateViewModel @Inject constructor(): ViewModel() {
+class ExpenseCreateUpdateViewModel @Inject constructor(
+    private val insertExpenseUseCase: InsertExpenseUseCase,
+    private val updateExpenseUseCase: UpdateExpenseUseCase,
+    @IOContext private val ioContext: CoroutineContext,
+) : ViewModel() {
+
+    // Exposed MutableLiveData for two-way data binding
+    val amount = MutableLiveData<Double>()
+    val description = MutableLiveData<String>()
+    val date = MutableLiveData<String>()
+    val paid = MutableLiveData<Boolean>()
+
+    private val _expenseCreateUpdateEvent = MutableLiveData<Event<ExpenseCreateUpdateState>>()
+    val expenseCreateUpdateEvent: LiveData<Event<ExpenseCreateUpdateState>> =
+        _expenseCreateUpdateEvent
+
+    private var expenseId: String? = null
+    private var isNewExpense = true
+
+    fun saveExpense() = viewModelScope.launch(ioContext) {
+        try {
+            val currentAmount = amount.value
+            val currentDescription = description.value
+            val currentDate = date.value
+            val currentPaid = paid.value
+
+            if (currentAmount != null &&
+                currentDescription != null &&
+                currentDate != null &&
+                currentPaid != null
+            ) {
+                val expenseItem = ExpenseItem(
+                    amount = BigDecimal(currentAmount),
+                    description = currentDescription,
+                    date = DateUtils.formatDate(
+                        currentDate,
+                        DateUtils.DAY_MONTH_YEAR_FORMAT_DATE_WITHOUT_TIME
+                    ),
+                    payed = currentPaid,
+                )
+
+                val currentExpenseId = expenseId
+                if (isNewExpense && currentExpenseId.isNullOrEmpty()) {
+                    insertExpenseUseCase(expenseItem)
+                } else {
+                    expenseItem.id = UUID.fromString(currentExpenseId)
+                    updateExpenseUseCase(expenseItem)
+                }
+                setSuccessState()
+            } else {
+                _expenseCreateUpdateEvent.postValue(
+                    Event(ExpenseCreateUpdateState.InvalidData)
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace() // Should be a log
+            setErrorState()
+        }
+    }
+
+    private fun setSuccessState() {
+        val state =
+            if (isNewExpense) ExpenseCreateUpdateState.SuccessInserting else ExpenseCreateUpdateState.SuccessUpdating
+        _expenseCreateUpdateEvent.postValue(Event(state))
+    }
+
+    private fun setErrorState() {
+        val state =
+            if (isNewExpense) ExpenseCreateUpdateState.ErrorInserting else ExpenseCreateUpdateState.ErrorUpdating
+        _expenseCreateUpdateEvent.postValue(Event(state))
+    }
+
+    sealed class ExpenseCreateUpdateState {
+        object SuccessInserting : ExpenseCreateUpdateState()
+        object ErrorInserting : ExpenseCreateUpdateState()
+        object SuccessUpdating : ExpenseCreateUpdateState()
+        object ErrorUpdating : ExpenseCreateUpdateState()
+        object InvalidData : ExpenseCreateUpdateState()
+    }
 
 }
