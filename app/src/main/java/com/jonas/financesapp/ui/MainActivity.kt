b423aadbb21fb2c44409a64ba38424121ebe220b
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2021-06-25 07:23:57 -0300

    feat(ui): setup navigation and create base fragments

--- app/src/main/java/com/jonas/financesapp/ui/MainActivity.kt
+++ app/src/main/java/com/jonas/financesapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jonas.financesapp
+package com.jonas.financesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jonas.financesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
