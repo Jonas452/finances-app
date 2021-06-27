c1b53fafa3b84c7632e0d736be01f6567afcb131
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2021-06-27 06:55:15 -0300

    fix(app): remove logic from ui

--- app/src/main/java/com/jonas/financesapp/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/jonas/financesapp/ui/dashboard/DashboardViewModel.kt
@@ -1,23 +1,25 @@
 package com.jonas.financesapp.ui.dashboard
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import android.content.Context
+import androidx.lifecycle.*
+import com.jonas.financesapp.R
 import com.jonas.financesapp.model.IncomeExpenseItem
 import com.jonas.financesapp.model.IncomeExpenseType
 import com.jonas.financesapp.usecase.expense.GetSumAllExpenseUseCase
 import com.jonas.financesapp.usecase.income.GetSumAllIncomeUseCase
 import com.jonas.financesapp.usecase.incomeexpense.GetAllIncomeExpenseUseCase
 import com.jonas.financesapp.util.Event
+import com.jonas.financesapp.util.LiveDataUtils
 import com.jonas.financesapp.util.roundTo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.map
 import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
     getAllIncomeExpenseUseCase: GetAllIncomeExpenseUseCase,
     getSumAllExpenseUseCase: GetSumAllExpenseUseCase,
     getSumAllIncomeUseCase: GetSumAllIncomeUseCase,
@@ -29,8 +31,24 @@ class DashboardViewModel @Inject constructor(
     val dashboardEvent: LiveData<Event<DashboardEvent>>
         get() = _dashboardEvent
 
-    val income = getSumAllIncomeUseCase().map { it.roundTo(2) }.asLiveData()
-    val expense = getSumAllExpenseUseCase().map { it.roundTo(2) }.asLiveData()
+    private val _income: LiveData<Double> =
+        getSumAllIncomeUseCase().asLiveData()
+    val income: LiveData<String>
+        get() = _income.map { context.getString(R.string.sign_money, it.roundTo(2).toString()) }
+
+    private val _expense: LiveData<Double> =
+        getSumAllExpenseUseCase().asLiveData()
+    val expense: LiveData<String>
+        get() = _expense.map { context.getString(R.string.sign_money, it.roundTo(2).toString()) }
+
+    private val _balance by lazy {
+        LiveDataUtils.combine(_income, _expense) { inc, exp ->
+            (inc ?: 0.0).minus(exp ?: 0.0)
+        }
+    }
+    val balance: LiveData<String>
+        get() = _balance.map { context.getString(R.string.sign_money, it.roundTo(2).toString()) }
+
 
     fun openIncomeExpense(incomeExpenseItem: IncomeExpenseItem) {
         when (incomeExpenseItem.type) {
